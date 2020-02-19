07a378c9bc627b20b9e5c5c460c56fdd724d539b
Nickolay Dudaryk <nick.dudaryk@gmail.com>
2020-02-19 21:55:53 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let positive = Math.abs(n),
+        positiveString = positive.toString(),
+        result = positiveString.split('').reverse().join('');
+
+    if (result[0] == '0'){
+        return (result.slice(1, result.length))
+    } else return (result);
 }
